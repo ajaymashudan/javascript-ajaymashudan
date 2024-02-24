49c49aafa32bdb6ad4ff11fae243d845f0f9e8c8
ajaymashudan <106131036+ajaymashudan@users.noreply.github.com>
2024-02-24 12:35:35 +0000

    convert

---
+++ basic/conversionOperator.js
@@ -0,0 +1,15 @@
+let score = "33";
+let num = "c";
+console.log(score);
+console.log(typeof score);
+console.log(typeof(score));
+let conNumber =Number(score);
+console.log(score);
+console.log(typeof conNumber);
+
+// null =0 
+// undefined = NaN
+// true = 1
+// false = 0 
+
+console.log(Number(num));
