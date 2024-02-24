30b44f2cfebce5963f020d0dd8a2c46b827e3166
ajaymashudan <106131036+ajaymashudan@users.noreply.github.com>
2024-02-24 12:19:55 +0000

    datatype and variable

---
+++ basic/dataType.js
@@ -0,0 +1,13 @@
+"use strict";
+// alert("hello"); //we are using node js not browser
+console.log(3+3)
+
+console.log("ajay")
+ 
+// Number => 2 to power 53
+// BigInt
+// String = "ajay"
+// null => object
+// undefined => undefined
+// Boolean  true/false
+// Symbol = unique
---
+++ basic/variable.js
@@ -0,0 +1,8 @@
+const accountId =145543;
+let accountEmail = "Ajay1234@gmail.com";
+var accountPassword = "Ajay@123";
+accointCity = "jaipur";
+let accountState; 
+// accountId = 2;
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accointCity,accountState]);
